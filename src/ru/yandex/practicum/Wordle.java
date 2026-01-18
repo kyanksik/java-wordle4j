04a35bf33b08def6c3d1c1fe658da6542d7a79a5
Boris <steamacc0unt@mail.ru>
2026-01-18 21:02:06 +0300

    first commit

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,18 +1,67 @@
 package ru.yandex.practicum;
 
-/*
-в главном классе нам нужно:
-    создать лог-файл (он должен передаваться во все классы)
-    создать загрузчик словарей WordleDictionaryLoader
-    загрузить словарь WordleDictionary с помощью класса WordleDictionaryLoader
-    затем создать игру WordleGame и передать ей словарь
-    вызвать игровой метод в котором в цикле опрашивать пользователя и передавать информацию в игру
-    вывести состояние игры и конечный результат
- */
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class Wordle {
-
     public static void main(String[] args) {
+        try (PrintWriter writer = createLogger()) {
+            writer.println("Запуск игры.");
 
+            WordleDictionaryLoader loader = new WordleDictionaryLoader(writer);
+            WordleDictionary dictionary = loadDictionary(loader, writer);
+
+            if (dictionary == null) {
+                writer.println("Словарь пуст");
+                return;
+            }
+
+            WordleGame wordleGame = new WordleGame(dictionary);
+            System.out.println("Игра началась");
+
+            playGame(wordleGame, writer);
+
+            writer.println("Игра завершена");
+        } catch (IOException e) {
+            System.err.println("Ошибка создания файла лога: " + e.getMessage());
+        }
     }
 
+    private static PrintWriter createLogger() throws IOException {
+        FileWriter fWrite = new FileWriter("logger.txt", StandardCharsets.UTF_8, true);
+        return new PrintWriter(fWrite);
+    }
+
+    private static WordleDictionary loadDictionary(
+            WordleDictionaryLoader loader,
+            PrintWriter writer
+    ) {
+        try {
+            return loader.loadFromFile("words_ru.txt");
+        } catch (IOException e) {
+            writer.println("Не удалось прочитать файл: " + e.getMessage());
+            System.err.println("Работа программы завершена");
+            return null;
+        }
+    }
+
+    private static void playGame(WordleGame game, PrintWriter writer) {
+        while (game.getSteps() > 0) {
+            int i = 1;
+            System.out.println(
+                    "Введите слово из " + WordleGame.WORD_LENGTH + " букв. Для подсказки нажмите Enter"
+            );
+
+            String message = game.checkAnswer();
+            System.out.println(message);
+                writer.println("Попыток осталось: " + game.getSteps() + " " + message);
+        }
+
+        if (game.getSteps() == 0) {
+            System.out.println("У вас закончились попытки.");
+            System.out.println("Правильный ответ: " + game.getAnswer());
+            writer.println("Закончились попытки");
+            writer.println("Ответ: " + game.getAnswer());
+        }
+    }
 }
